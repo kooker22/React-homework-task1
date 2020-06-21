69b57ddfd8a419a03df744616549446ccf127460
Иван Попович <kooker22>
2020-06-21 19:00:41 +0300

    Updates

---
+++ precache-manifest.b01e6fadc7530694d472f5d70ee2e195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce62206ad61ed102da12023f99037a4f",
+    "url": "/React-homework-task1/index.html"
+  },
+  {
+    "revision": "44c2f87e52bfb8920554",
+    "url": "/React-homework-task1/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "76be3a95ebac0632b259",
+    "url": "/React-homework-task1/static/css/main.2283c47b.chunk.css"
+  },
+  {
+    "revision": "44c2f87e52bfb8920554",
+    "url": "/React-homework-task1/static/js/2.24805c74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-homework-task1/static/js/2.24805c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76be3a95ebac0632b259",
+    "url": "/React-homework-task1/static/js/main.08faa250.chunk.js"
+  },
+  {
+    "revision": "111228b1486ac0a81465",
+    "url": "/React-homework-task1/static/js/runtime-main.a82ac5bc.js"
+  }
+]);
